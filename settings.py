0df3b876273c7f1386ec021f05b1f5d7a5241317
yellowchihuahua <47878958+yellowchihuahua@users.noreply.github.com>
2020-08-24 21:27:29 +0800

    hmm

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ import os
 COMMAND_PREFIX = "!"
 
 # The bot token. Keep this secret!
-BOT_TOKEN = "Don't let others know your token!"
+BOT_TOKEN = "NzQ3NDMzMTA3NDU1NDc1NzQ1.X0OzUw.TbvS2rLZDfGucYoRbAveGTeWDng"
 
 # The now playing game. Set this to anything false-y ("", None) to disable it
 NOW_PLAYING = COMMAND_PREFIX + "commands"
